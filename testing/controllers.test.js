afc0898a6e0894b7a48c7c1b2fda98306cacb288
hasanshahid99 <shahidhasan148@gmail.com>
2023-09-13 00:41:57 +0100

    2 tests on controller for users

--- testing/controllers.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../app') // Replace with the actual import for your Express app
-const Goal = require('../models/Goal'); // Replace with the actual import for your Goal model
-
-    describe('GET /goals', () => {
-        it('should return a list of goals with a 200 status code', async () => {
-          // Mock the getAll method of the Goal model to return sample data
-          jest.spyOn(Goal, 'getAll').mockResolvedValue([
-         
